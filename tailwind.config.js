8cbdf1f530f30c348deecede0cb1f679658290ce
unknown <aldrin.brinas@frontlinebusiness.com.ph>
2025-06-30 15:37:07 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#192f57",
+          secondary: "#4668a5",
+        },
+        fontFamily: {
+          poppins: "Poppins",
+        },
+      },
+    },
+    plugins: [],
+  }
